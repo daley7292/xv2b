faf8993d5c27b17c09f0fd0cd1701abad308651c
daley7292 <daley7292@gmail.com>
2025-07-09 13:34:47 +0800

    Create SubscribeLog.php

---
+++ app/Models/SubscribeLog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubscribeLog extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'v2_subscribe_log';
+
+    protected $fillable = [
+        'user_id',
+        'email',
+        'ip',
+        'country',
+        'city',
+        'user_agent',
+        'created_at',
+    ];
+}
